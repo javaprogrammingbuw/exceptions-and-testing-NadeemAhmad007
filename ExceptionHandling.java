976c8b3534897ba3463d0d92213f06857ee6fa98
Nadeem Ahmad <nibi0629@in.uni-weimar.de>
2019-03-28 11:53:35 +0100

    updated

--- ExceptionHandling.java
+++ ExceptionHandling.java
@@ -1,9 +1,6 @@
 
 public class ExceptionHandling {
-
-	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws MyException {
 		//todo:
 		//1.) Execute the following program. It will throw Exceptions.
 		//Catch the Exceptions using their proper Names till no further
@@ -13,19 +10,38 @@ public class ExceptionHandling {
 		//the exception. Write your own Exception for this case and 
 		//write a method which throws this exception
 
-		int a = 5;
-		int b = 0;
-		int div = a/b;
-		
-		int[] arr = new int[5];
-		arr[5] = 7;
-		
-		String one = "one";
-		int i = Integer.parseInt(one);
-		
-		double c = -7.0;
-		double d = Math.sqrt(c);
-		System.out.println(d);
-	}
+		try{
+			int a = 5;
+			int b = 0;
+			int div = a/b;
+		} catch (ArithmeticException e){
+			System.err.println( "Oops ... division by zero is not supported." );
+			//e.printStackTrace();
+		} 
 
+		try{
+			int[] arr = new int[5];
+			arr[5] = 7;
+		} catch (ArrayIndexOutOfBoundsException e){
+			e.printStackTrace();
+		}
+		
+		try{
+			String one = "one";
+			int i = Integer.parseInt(one);
+		} catch (NumberFormatException e){
+			e.printStackTrace();
+		}
+		
+		try{
+			double c = -7.0;
+			if( c < 0.0 ) {
+				throw new MyException("Result  is negative ");
+			}
+			double d = Math.sqrt(c);
+			System.out.println(d);
+		} catch (MyException e) {
+			e.printStackTrace();
+		}
+	}
 }
